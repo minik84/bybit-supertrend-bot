cbb54eb46ad9095293b08f22a501efe7f3e27162
minik84 <32544418+minik84@users.noreply.github.com>
2025-07-27 16:44:43 +0200

    Update supertrend_strategy_wifi_trader.py

--- supertrend_strategy_wifi_trader.py
+++ supertrend_strategy_wifi_trader.py
@@ -24,9 +24,17 @@ BOT_CONFIGS = [
         "interval": "15",
         "atr_period": 10,
         "factor": 3.0,
-        "risk_percentage": 5,
+        "risk_percentage": 3,
         "take_profit_percentage": 1.5
-    }
+    },
+    {
+        "symbol": "1000BONKUSDT",
+        "interval": "15",
+        "atr_period": 10,
+        "factor": 3.0,
+        "risk_percentage": 3,
+        "take_profit_percentage": 1.5
+    },
 ]
 # ==============================================================================
 
